8e3801da418df6e2babcbfdfc4bc2dc1ce991d64
Robin <jake.cramer04@gmail.com>
2025-01-09 16:43:53 -0500

    css cleanup made

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,5 @@
 import './index.css'
+import './Footer.css'
 
 export default function Footer(){
     return(
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './index.css'
+import './Header.css'
 
 
 function DropButton({changeSite}){
